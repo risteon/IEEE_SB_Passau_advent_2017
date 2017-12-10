8d26cb3848780ded55038e049cad03449cb3d61f
Christoph Rist <rist.christoph@gmail.com>
2017-12-10 19:56:31 +0100

    Another try 05

--- 05_christmas_garland_decorating.py
+++ 05_christmas_garland_decorating.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python3
+import operator
 
 __author__ = "Christoph Rist"
 __license__ = "MIT"
 
 
 def find_min_costs(costs, diffs):
-    if not costs:
-        return 0
-
-    for index, diff in enumerate(diffs):
-        if diff > 0:
-            return costs[index][1]\
-                + find_min_costs(costs[:index], diffs[:index])\
-                + find_min_costs(costs[index+1:], diffs[index+1:])
 
     costs_green, costs_red = zip(*costs)
-    return min(sum(costs_green[::2]) + sum(costs_red[1::2]),
+    smallest = min(sum(costs_green[::2]) + sum(costs_red[1::2]),
             sum(costs_red[::2]) + sum(costs_green[1::2]))
+
+    max_index, _ = max(enumerate(diffs), key=operator.itemgetter(1))
+
+    cost = costs_green[max_index]
+    if max_index > 0:
+        cost += costs_red[max_index-1]
+    if max_index < len(costs) - 1:
+        cost += costs_red[max_index+1]
+
+    if max_index > 1:
+        cost += find_min_costs(costs[:max_index-1], diffs[:max_index-1])
+    if max_index < len(costs) - 2:
+        cost += find_min_costs(costs[max_index+2:], diffs[max_index+2:])
+
+    return min(smallest, cost)
 
 
 def main():
@@ -26,10 +34,22 @@ def main():
     costs = []
     for _ in range(n):
         costs.append(tuple(map(int, input().split())))
-    diffs = list(map(lambda x: x[0] - x[1], costs))
-    print(find_min_costs(costs, diffs))
+    diffs = list(map(lambda x: x[1] - x[0], costs))
+
+    begin = 0
+    total = 0
+    for index, diff in enumerate(diffs):
+        if diff <= 0:
+            if index > begin:
+                total += find_min_costs(costs[begin:index], diffs[begin:index])
+            total += costs[index][1]
+            begin = index + 1
+
+    if begin != len(diffs):
+        total += find_min_costs(costs[begin:], diffs[begin:])
+
+    print(total)
 
 
 if __name__ == "__main__":
     main()
-
