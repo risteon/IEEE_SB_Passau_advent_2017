cc502f83e63ff1a7c83311fd741a96d0d55b9f8f
Christoph Rist <rist.christoph@gmail.com>
2017-12-10 21:06:50 +0100

    Work in progress

--- 05_christmas_garland_decorating.py
+++ 05_christmas_garland_decorating.py
@@ -1,52 +1,70 @@
 #!/usr/bin/env python3
 import operator
+import itertools
+import sys
 
 __author__ = "Christoph Rist"
 __license__ = "MIT"
 
 
-def find_min_costs(costs, diffs):
+def generate_perm_lists(target_sums):
+    yield[2, 2, 2, 2]
 
-    costs_green, costs_red = zip(*costs)
-    smallest = min(sum(costs_green[::2]) + sum(costs_red[1::2]),
-            sum(costs_red[::2]) + sum(costs_green[1::2]))
 
-    max_index, _ = max(enumerate(diffs), key=operator.itemgetter(1))
+def find_min_costs(diffs):
 
-    cost = costs_green[max_index]
-    if max_index > 0:
-        cost += costs_red[max_index-1]
-    if max_index < len(costs) - 1:
-        cost += costs_red[max_index+1]
+    if not diffs:
+        raise RuntimeError("wrong diffs:empty")
+    for d in diffs:
+        if d >= 0:
+            raise RuntimeError("wrong diffs")
 
-    if max_index > 1:
-        cost += find_min_costs(costs[:max_index-1], diffs[:max_index-1])
-    if max_index < len(costs) - 2:
-        cost += find_min_costs(costs[max_index+2:], diffs[max_index+2:])
+    to_reach = [len(diffs), len(diffs) + 1]
 
-    return min(smallest, cost)
+    min_cost = 0
+
+    for l in generate_perm_lists(to_reach):
+        for perm in itertools.permutations(l):
+            index = -2
+            c = 0
+            for p in perm:
+                index += p
+                c += diffs[index]
+            if c < min_cost:
+                min_cost = c
+
+    return min_cost
 
 
 def main():
     """
     """
+    if True:
+        sys.stdin = open("samples/05.1_input.txt")
+
     n = int(input())
-    costs = []
+    costs_green = []
+    costs_red = []
     for _ in range(n):
-        costs.append(tuple(map(int, input().split())))
-    diffs = list(map(lambda x: x[1] - x[0], costs))
+        g, r = map(int, input().split())
+        costs_green.append(g)
+        costs_red.append(r)
+    diffs = [a - b for a, b, in zip(costs_green, costs_red)]
 
     begin = 0
     total = 0
     for index, diff in enumerate(diffs):
-        if diff <= 0:
+        if diff >= 0:
             if index > begin:
-                total += find_min_costs(costs[begin:index], diffs[begin:index])
-            total += costs[index][1]
+                total += find_min_costs(diffs[begin:index])
+                total += sum(costs_red[begin:index])
+
+            total += costs_red[index]
             begin = index + 1
 
     if begin != len(diffs):
-        total += find_min_costs(costs[begin:], diffs[begin:])
+        total += find_min_costs(diffs[begin:])
+        total += sum(costs_red[begin:])
 
     print(total)
 
